5391d2f20206c0203d71f19d4a0c92a3248ec6d1
s20041 <s20041@pjwstk.edu.pl>
2019-05-25 12:44:41 +0200

    0.01

---
+++ Laboratorium6/zad4/DemoRunners.java
@@ -0,0 +1,13 @@
+public class DemoRunners {
+
+    public static void main(String args[]){
+        Athlete atleta = new Athlete();
+        Machine machina = new Machine();
+        PoliticalCandidate candidate = new PoliticalCandidate();
+
+        atleta.run();
+        machina.run();
+        candidate.run();
+
+    }
+}
