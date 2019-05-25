bbc4d3a186ebde7a9c8149b2a32374ebb6082631
s20041 <s20041@pjwstk.edu.pl>
2019-05-25 11:42:43 +0200

    0.01

---
+++ Laboratorium6/zad1/MovablePoint.java
@@ -0,0 +1,33 @@
+public class MovablePoint implements  Movable{
+
+    int x, y, xSpeed, ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed){
+
+    }
+
+    public String toString(){
+        return "x: "+x+" y: "+y+
+                " xSpeed: "+xSpeed+" ySpeed: "+ySpeed;
+    }
+
+    @Override
+    public void moveUp() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void moveLeft() {
+
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
+}
---
+++ Laboratorium6/zad1/MovableRectangle.java
@@ -0,0 +1,49 @@
+public class MovableRectangle extends MovablePoint implements Movable {
+
+    int x1, y1, x2, y2, xSpeed, ySpeed;
+
+    public MovableRectangle(int x, int y, int x1, int y1, int x2, int y2, int xSpeed,int ySpeed){
+        super(x,y,xSpeed,ySpeed);
+        this.x1=x1;
+        this.y1=y1;
+        this.x2=x2;
+        this.y2=y2;
+        this.xSpeed=xSpeed;
+        this.ySpeed=ySpeed;
+    }
+
+    MovablePoint topLeft;
+    MovablePoint bottomRight;
+
+    @Override
+    public void moveUp() {
+        topLeft.ySpeed-=topLeft.ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        topLeft.ySpeed+=topLeft.ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        topLeft.xSpeed-=topLeft.xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        topLeft.xSpeed+=topLeft.xSpeed;
+    }
+
+    public String toString(){
+        return "x1: "+x1+" y1: "+y1+
+                " x2: "+x2+"y2: "+y2+
+                " xSpeed: "+xSpeed+" ySpeed: "+ySpeed;
+    }
+
+    public static void main(String[] args) {
+
+        MovableRectangle test = new MovableRectangle(0,0,1,1,2,2,5,5);
+    }
+
+}
