b576dfe6b595969c88235824632ddf2beaecf5db
s20041 <s20041@pjwstk.edu.pl>
2019-06-08 12:48:53 +0200

    0.02

---
+++ Laboratorium6/zad6/LoanConstants.java
@@ -0,0 +1,11 @@
+public interface LoanConstants {
+
+    public interface LoanConstant {
+        public final static int SHORT_TERM = 1;
+        public final static int MEDIUM_TERM = 3;
+        public final static int LONG_TERM = 5;
+        public final static String COMPANY_NAME = "Sanchez Construction";
+        public final static int MAX_LOAN_AMOUNT = 100000;
+    }
+
+}
